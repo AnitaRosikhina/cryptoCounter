c485e827d245f78e68a3a9c99a40410c57fab1f9
Anita Rosikhina <anita2.sergeevna@gmail.com>
2021-12-23 12:14:06 +0200

    - cleaned up the default code

--- apps/crypto-counter/src/app/app.component.ts
+++ apps/crypto-counter/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@crypto/api-interfaces';
 
 @Component({
   selector: 'crypto-root',
@@ -8,6 +6,4 @@ import { Message } from '@crypto/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
 }
